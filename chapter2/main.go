40c427a9165c7ebb13d00374853b8ebb072c9d58
ant <anth1y@gmail.com>
2016-05-19 09:27:30 -0400

    Inital commt

---
+++ chapter2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+  fmt.Println("Hello, " + " my name is Ant")
+  fmt.Println(len("Hello, my name is Ant"))
+  fmt.Println("Hello, my name is Ant"[1])
+}
